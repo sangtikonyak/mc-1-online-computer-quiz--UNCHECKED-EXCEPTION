e2cd0fa9849ef3fbef0b685aaa989c18f25e4914
bhakti.rathod <bhakti.rathod@stackroute.in>
2022-07-14 16:32:09 +0530

    Initial commit

---
+++ src/main/java/com/jap/QuizDemo.java
@@ -0,0 +1,26 @@
+package com.jap;
+
+public class QuizDemo {
+
+    //return the school name which has got the highest score in quiz
+    //Handle the NumberFormatException
+    public String highestScore(String []nameOfSchool , String[] scores ){
+
+
+        return "";
+
+    }
+    //convert all the names in uppercase
+    //Handle the NullPointerException
+    public String[] convertAllNamesToCapital(String name[]){
+        String upperCase[] = new String[0];
+
+        return upperCase;
+    }
+}
+
+
+
+
+
+
