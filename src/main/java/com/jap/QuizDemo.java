039b7e499228a747f4304f933b7495f281b2c9ac
Sangti konyak <ssangtikonyak@gmail.com>
2022-10-16 23:51:37 +0530

    commit 1

--- src/main/java/com/jap/QuizDemo.java
+++ src/main/java/com/jap/QuizDemo.java
@@ -1,21 +1,59 @@
 package com.jap;
 
+import java.util.Arrays;
+
 public class QuizDemo {
 
     //return the school name which has got the highest score in quiz
     //Handle the NumberFormatException
     public String highestScore(String []nameOfSchool , String[] scores ){
-
-
-        return "";
+         int max=0;
+        String schoolName="";
+        try {
+            for (int i = 0; i < scores.length; i++) {
+                if (max < Integer.parseInt(scores[i])) {
+                    max = Integer.parseInt(scores[i]);
+                   schoolName = nameOfSchool[i];//.concat("-" + String.valueOf(max));
+                }
+            }
+        }catch (NumberFormatException e) {
+            return e.toString();
+        }catch(ArrayIndexOutOfBoundsException e)
+        {
+            e.printStackTrace();
+            System.out.println("Check array length");
+            return e.toString();
+        }catch (NullPointerException e)
+        {
+            e.printStackTrace();
+            System.out.println("Name of school cannot be a null");
+            return e.toString();
+        }
+        return schoolName;
 
     }
     //convert all the names in uppercase
     //Handle the NullPointerException
-    public String[] convertAllNamesToCapital(String name[]){
-        String upperCase[] = new String[0];
+    public String[] convertAllNamesToCapital(String[] name){
+        String[] upperCase = new String[name.length];
+        for (int i=0;i< name.length;i++)
+        {
+            upperCase[i]=name[i].toUpperCase();
+
+        }
+
 
         return upperCase;
+    }
+
+    public static void main(String[] args) {
+        String[] nameOfSchool=new String[]{"kscj","sjs","lfs","dbs"};
+        String[] scores=new String[]{"20","23","66","55"};
+
+        QuizDemo quizDemo=new QuizDemo();
+        System.out.println("quizDemo.highestScore(nameOfSchool,scores) = " + quizDemo.highestScore(nameOfSchool, scores));
+
+        System.out.println("quizDemo.convertAllNamesToCapital(nameOfSchool) = " + Arrays.toString(quizDemo.convertAllNamesToCapital(nameOfSchool)));
     }
 }
 
